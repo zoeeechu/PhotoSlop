d000b4bcc18c51c6e35af23839844aac23e59b18
Novel Alex <novelalex29@outlook.com>
2026-01-31 19:03:28 -0500

    Websocket connected on icon and name

--- src/CharacterSelector.svelte
+++ src/CharacterSelector.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
   const characters = ['ava', 'cubert', 'potat'];
+  export let selected: string | null = null;
+  export let onSelect: (character: string) => void;
 </script>
 
 <div class="flex gap-4">
   {#each characters as character}
-    <img src="/images/characters/{character}.png" alt={character} 
-    class="w-24 h-24 object-contain  hover:scale-110 hover:outline-blue-500 transition-transform cursor-pointer" />
+    <img 
+      src="/images/characters/{character}.png" 
+      alt={character} 
+      class="w-24 h-24 object-contain hover:scale-110 transition-transform cursor-pointer {selected === character ? 'outline outline-4 outline-blue-500' : ''}"
+      on:click={() => { selected = character; onSelect(character); }}
+    />
   {/each}
 </div>
